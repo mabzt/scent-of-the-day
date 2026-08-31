24d216faccbed430494f472ceb250ec92982fdc7
Thato Mabeleng <tlmabeleng@gmail.com>
2026-08-31 06:59:56 +0200

    configure OpenWeather Resdt client

---
+++ src/main/java/com/maison/mabs/sotd/infrastructure/config/openweather/OpenWeatherClientConfig.java
@@ -0,0 +1,32 @@
+package com.maison.mabs.sotd.infrastructure.config.openweather;
+
+import com.maison.mabs.sotd.infrastructure.adapter.out.client.OpenWeatherClientApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(OpenWeatherConfigProperties.class)
+public class OpenWeatherClientConfig {
+
+    private final OpenWeatherConfigProperties configProperties;
+
+    @Bean
+    public OpenWeatherClientApi openWeatherClientApi(RestClient.Builder builder) {
+        RestClient restClient = builder.baseUrl(this.configProperties.baseUrl())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
+                .build()
+                .createClient(OpenWeatherClientApi.class);
+
+    }
+}
