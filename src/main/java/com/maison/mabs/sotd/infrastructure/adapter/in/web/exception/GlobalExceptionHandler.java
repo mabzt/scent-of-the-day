98650101faa41fd390428c0926af6ca4f1348cc2
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-03 20:40:27 +0200

    handle conflicts

--- src/main/java/com/maison/mabs/sotd/infrastructure/adapter/in/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/maison/mabs/sotd/infrastructure/adapter/in/web/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.maison.mabs.sotd.infrastructure.adapter.in.web.exception;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -27,6 +29,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 							Objects.requireNonNullElse(fieldError.getDefaultMessage(), "invalid")))
 					.toList());
 		return ResponseEntity.of(problemDetail).build();
+	}
+
+	@ExceptionHandler(SotdConflictException.class)
+	public ResponseEntity<ProblemDetail> handleConflict(SotdConflictException exception) {
+		var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, exception.getMessage());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
 	}
 
 }
