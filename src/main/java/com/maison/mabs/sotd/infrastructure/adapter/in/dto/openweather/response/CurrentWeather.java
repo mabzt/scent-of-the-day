d2922d02e8781f72971d5a39204d3bc00bcf236f
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-06 07:51:27 +0200

    add builder annotation

--- src/main/java/com/maison/mabs/sotd/infrastructure/adapter/in/dto/openweather/response/CurrentWeather.java
+++ src/main/java/com/maison/mabs/sotd/infrastructure/adapter/in/dto/openweather/response/CurrentWeather.java
@@ -2,10 +2,12 @@ package com.maison.mabs.sotd.infrastructure.adapter.in.dto.openweather.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
 import java.time.Instant;
 import java.util.List;
 
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CurrentWeather(
 
