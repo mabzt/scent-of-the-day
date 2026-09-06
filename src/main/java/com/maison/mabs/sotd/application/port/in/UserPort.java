65f14c881929fb16d5dda0a7dfc2c8735a64ebac
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-06 07:52:50 +0200

    remove framework annotations in use cases

--- src/main/java/com/maison/mabs/sotd/application/port/in/UserPort.java
+++ src/main/java/com/maison/mabs/sotd/application/port/in/UserPort.java
@@ -3,15 +3,13 @@ package com.maison.mabs.sotd.application.port.in;
 import com.maison.mabs.sotd.domain.model.User;
 import com.maison.mabs.sotd.infrastructure.adapter.in.dto.user.request.CollectionRequest;
 import com.maison.mabs.sotd.infrastructure.adapter.in.dto.user.request.CreateUserRequest;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public interface UserPort {
 
-	User createUserProfile(@Valid @NotNull CreateUserRequest createUserRequest);
+	User createUserProfile(CreateUserRequest createUserRequest);
 
-	User addCollection(@NotNull UUID id, @Valid @NotNull CollectionRequest collectionRequest);
+	User addCollection(UUID id, CollectionRequest collectionRequest);
 
 }
--- src/main/java/com/maison/mabs/sotd/application/port/out/OpenWeatherPort.java
+++ src/main/java/com/maison/mabs/sotd/application/port/out/OpenWeatherPort.java
@@ -2,15 +2,14 @@ package com.maison.mabs.sotd.application.port.out;
 
 import com.maison.mabs.sotd.infrastructure.adapter.in.dto.openweather.response.CurrentWeather;
 import com.maison.mabs.sotd.infrastructure.adapter.in.dto.openweather.response.Location;
-import lombok.NonNull;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface OpenWeatherPort {
 
-	List<Location> getLocation(@NonNull String city);
+	List<Location> getLocation(String city);
 
-	CurrentWeather getCurrentWeather(@NonNull BigDecimal longitude, @NonNull BigDecimal latitude);
+	CurrentWeather getCurrentWeather(BigDecimal longitude, BigDecimal latitude);
 
 }
