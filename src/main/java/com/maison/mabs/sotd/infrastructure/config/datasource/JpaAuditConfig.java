252b0084c85ecd849746f1315784381c64a0582f
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-06 07:51:48 +0200

    add jpa auditor config

---
+++ src/main/java/com/maison/mabs/sotd/infrastructure/config/datasource/JpaAuditConfig.java
@@ -0,0 +1,10 @@
+package com.maison.mabs.sotd.infrastructure.config.datasource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+
+}
