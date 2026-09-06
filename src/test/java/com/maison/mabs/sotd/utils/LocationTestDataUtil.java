166402d8b02365864631c747aab96462cd112a2e
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-06 07:53:27 +0200

    add test data utils

--- src/test/java/com/maison/mabs/sotd/utils/LocationTestDataUtil.java
+++ src/test/java/com/maison/mabs/sotd/utils/LocationTestDataUtil.java
@@ -1,5 +1,6 @@
 package com.maison.mabs.sotd.utils;
 
+import com.maison.mabs.sotd.domain.model.UserLocation;
 import com.maison.mabs.sotd.infrastructure.adapter.in.dto.openweather.response.Location;
 import lombok.experimental.UtilityClass;
 
@@ -15,6 +16,19 @@ public class LocationTestDataUtil {
 			.longitude(BigDecimal.valueOf(28.125912))
 			.country("ZA")
 			.province("Gauteng")
+			.build();
+	}
+
+	public UserLocation validUserLocation() {
+		return UserLocation.builder()
+			.city("Midrand")
+			.latitude(BigDecimal.valueOf(-25.999262))
+			.longitude(BigDecimal.valueOf(28.125912))
+			.country("ZA")
+			.province("Gauteng")
+			.currentTemperature(BigDecimal.valueOf(20.03))
+			.minimumTemperature(BigDecimal.valueOf(13.4))
+			.maximumTemperature(BigDecimal.valueOf(29.4))
 			.build();
 	}
 
